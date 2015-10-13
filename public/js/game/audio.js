aebe084380fcb778c4b26de74afbc680674a36b8
Nikolaj Zamulov <zamulov8@gmail.com>
2015-10-13 11:21:22 +0300

    Add sounds in .mp3 format for IE 11 correctly working.

--- public/js/game/audio.js
+++ public/js/game/audio.js
@@ -8,17 +8,17 @@
 
  
 var laser_weapon_1_sound = new Howl({
-	urls: ['public/sounds/laser_weapon_1.wav']
+	urls: ['public/sounds/laser_weapon_1.wav', 'public/sounds/laser_weapon_1.mp3']
 });
 
 var explosion_sound = new Howl({
-	urls: ['public/sounds/explosion.wav']
+	urls: ['public/sounds/explosion.wav', 'public/sounds/explosion.mp3']
 });
 
 var restore_ammunition_sound = new Howl({
-	urls: ['public/sounds/restore_ammunition.wav']
+	urls: ['public/sounds/restore_ammunition.wav', 'public/sounds/restore_ammunition.mp3']
 });
 
 var restore_health_sound = new Howl({
-	urls: ['public/sounds/restore_health.wav']
+	urls: ['public/sounds/restore_health.wav', 'public/sounds/restore_health.mp3']
 });
