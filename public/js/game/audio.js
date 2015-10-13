013846c67154a2255fa2aacee0d8338ff1016b72
Nikolaj Zamulov <zamulov8@gmail.com>
2015-10-14 00:51:48 +0300

    Add .ogg files into audio.js.

--- public/js/game/audio.js
+++ public/js/game/audio.js
@@ -8,17 +8,17 @@
 
  
 var laser_weapon_1_sound = new Howl({
-	urls: ['public/sounds/laser_weapon_1.wav', 'public/sounds/laser_weapon_1.mp3']
+	urls: ['public/sounds/laser_weapon_1.wav', 'public/sounds/laser_weapon_1.mp3', 'public/sounds/laser_weapon_1.ogg']
 });
 
 var explosion_sound = new Howl({
-	urls: ['public/sounds/explosion.wav', 'public/sounds/explosion.mp3']
+	urls: ['public/sounds/explosion.wav', 'public/sounds/explosion.mp3', 'public/sounds/explosion.ogg']
 });
 
 var restore_ammunition_sound = new Howl({
-	urls: ['public/sounds/restore_ammunition.wav', 'public/sounds/restore_ammunition.mp3']
+	urls: ['public/sounds/restore_ammunition.wav', 'public/sounds/restore_ammunition.mp3', 'public/sounds/restore_ammunition.ogg']
 });
 
 var restore_health_sound = new Howl({
-	urls: ['public/sounds/restore_health.wav', 'public/sounds/restore_health.mp3']
+	urls: ['public/sounds/restore_health.wav', 'public/sounds/restore_health.mp3', 'public/sounds/restore_health.ogg']
 });
