dc5910e62ec58c0e8f4a768f0954e814e94282a4
Nikolaj Zamulov <zamulov8@gmail.com>
2015-10-19 22:40:39 +0300

    Make some changes in bulletsPull.js

--- public/js/game/bulletsPull.js
+++ public/js/game/bulletsPull.js
@@ -2,19 +2,12 @@ var BulletsPull = function() {
 
 	this.bullets = [];
 
-	(function(bullets) {
-		for(var i = 0; i < 500; ++i) {
-			bullets.push(Crafty.e('2D, Canvas'));
-			bullets[bullets.length - 1].attr({ x: -1, y: -1 });
-		}
-	})(this.bullets);
-
 	this.resize = function() {
 		for(var i = 0; i < 50; ++i) {
 			this.bullets.push(Crafty.e('2D, Canvas'));
 			this.bullets[this.bullets.length - 1].attr({ x: -1, y: -1 });
 		}
-	}
+	};
 
 	this.getBullet = function(type) {
 		if(0 === this.bullets.length) {
@@ -25,11 +18,17 @@ var BulletsPull = function() {
 		bullet.type = type;
 		bullet.origin('center');
 		return bullet;
-	}
+	};
 
 	this.returnBullet = function(bullet) {
 		bullet.removeComponent(bullet.type);
 		bullet.attr({ x: -1, y: -1 });
 		this.bullets.push(bullet);
-	}
+	};
+
+	(function(that) {
+		for(var i = 0; i < 10; ++i) {
+			that.resize();
+		}
+	})(this);
 }
